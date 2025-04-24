016abe6b4963a62318c39585cf726abb96d30ba7
mohammedaliyu136 <mohammedaliyu136@gmail.com>
2025-04-24 14:17:52 +0100

    Update settings.py

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = 'django-insecure-fv@!-bs_yfk2!)c!8%$=qvhyxd)yfbo4z6cz&(eo@v6p2es7@6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
